d4521d42de7baa387bcee2bd8ab839be6088008a
César López Ramírez <cesar.lopez@somenergia.coop>
2016-01-26 18:38:58 +0100

    First AJAX version

--- scriptlauncher.py
+++ scriptlauncher.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-from flask import Flask, request, Response, render_template, redirect, abort
+from flask import Flask, request, Response, render_template, redirect, abort, flash, request
 from wtforms import StringField,SubmitField,FieldList
 from wtforms.validators import DataRequired
 from flask_wtf import Form
 from yamlns import namespace as ns
 from ooop import OOOP
+from collections import OrderedDict
 import functools
 import subprocess
 import deansi
@@ -14,14 +15,23 @@ scripts = ns.load('scripts.yaml')
 
 
 class ParameterForm(Form):
-    
+
     parms = FieldList(StringField('Parameter'))
     submit = SubmitField("Execute")
+
     def validate(self):
-        try:
-            return all(val(parm.data) for parm,val in zip(self.parms,self.validations))
-        except Exception:
-            return False
+        validation=True
+        for parm,(parmname,val) in zip(self.parms,self.validations.iteritems()):
+            try:
+                if not val(parm.data):
+                    print "validate: "+parm
+                    self.errors[parm]='Invalid parameter {} '.format(parmname)
+                    validation=False
+            except Exception:
+                print(repr(parm))
+                self.errors[parm]='Invalid parameter {} '.format(parmname)
+                validation=False
+        return validation
 
 def requires_auth(f):
     @functools.wraps(f)
@@ -54,29 +64,40 @@ def check_auth(username, password):
         print "Unable to connect to ERP"
         return False
 
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        flash(u"Error in the %s field - %s" % (
+            field.label.text,
+            errors
+        ))
 
 @app.route('/', methods=('GET', 'POST'))
 @requires_auth
 def index():
     scripts = ns.load('scripts.yaml')
     forms={}
-    for key in scripts.iterkeys():
-        forms[key]=ParameterForm(prefix=key)   
-        if 'parameters' in scripts[key]:
-            forms[key].validations=[eval('lambda parm: '+str(val_func)) for val_func in scripts[key].parameters.itervalues()]
-        else:
-            forms[key].validations=[]
-        if forms[key].validate_on_submit() and forms[key].submit.data:
-            parameters="&".join([a.data for a in forms[key].parms])
-            return redirect('/run/'+key+'/'+parameters) if parameters else redirect('/run/'+key)
-    return render_template('index_template.html',items=scripts.iteritems(),forms=forms)
+    return render_template('index_template.html',items=scripts.iteritems())
+
+
+@app.route('/runner/<cmd>')
+@requires_auth
+def runner(cmd):
+    script=scripts[cmd]
+    return render_template(
+        'runner_template.html',
+        name=cmd,
+        style=deansi.styleSheet(),
+        **script
+        )
 
 def execute(scriptname,parms=""):
     output=subprocess.check_output(
         scripts[scriptname].script+parms,
         stderr=subprocess.STDOUT,
         shell=True).decode('utf-8')
+    return deansi.deansi(output)
     return render_template('cmd_template.html',script_name=scriptname,script=scripts[scriptname].script,script_output=deansi.deansi(output),style=deansi.styleSheet())
+
 
 @app.route('/run/<scriptname>')
 @requires_auth
@@ -84,6 +105,8 @@ def script_without_parms(scriptname):
     if 'parameters' in scripts[scriptname]:
         abort(400)
     return execute(scriptname)
+
+
 @app.route('/run/<scriptname>/<parameters>')
 @requires_auth
 def script_with_parms(scriptname,parameters):
@@ -92,9 +115,16 @@ def script_with_parms(scriptname,parameters):
         abort(400)
     param_spaced=' '+parameters.replace('&',' ')
     return execute(scriptname,parms=param_spaced)
+
+
 @app.errorhandler(400)
 def badRequest(e):
     return render_template('400.html')
+
+
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?Rd'
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', processes=8)
+
