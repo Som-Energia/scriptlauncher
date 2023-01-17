201eef1cb7ec732bbe334aedc0836f839dd4714b
David García Garzón <david.garcia@somenergia.coop>
2023-01-17 07:12:22 +0100

    b2b tests updated

--- test.sh
+++ test.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
+function b2b {
+	name="$1"
+	"${@:2}" > b2b/"$name"-result &&
+		diff -q b2b/"$name"-result b2b/"$name"-expected 2> /dev/null &&
+		echo -e '\033[32mOK\033[34;1m '"$name"'\033[0m' ||
+		echo -e '\033[31mKO\033[34;1m '"$name"'\033[0m Run:' diff b2b/"$name"-result b2b/"$name"-expected
+}
+
 auth=$(cat test.cfg)
-curl localhost:5000 -u $auth >  b2b/index-result.html && diff b2b/index-expected.html b2b/index-result.html 2> /dev/null  && echo OK || echo KO
-curl localhost:5000/runner/test -u $auth >  b2b/runner-result.html && diff b2b/runner-expected.html b2b/runner-result.html 2> /dev/null  && echo OK || echo KO
-curl localhost:5000/run/test -u $auth >  b2b/run-result.html && diff b2b/run-expected.html b2b/run-result.html 2> /dev/null  && echo OK || echo KO
+b2b index \
+	curl -s localhost:5000 -u $auth
+b2b runner \
+	curl -s localhost:5000/runner/test_parameter -u $auth
+b2b run \
+	curl -s localhost:5000/run/test_parameter -u $auth
+b2b workingdirOverride \
+	curl -s localhost:5000/run/workingdirOverride -u $auth
+b2b workingdirLegacy \
+	curl -s localhost:5000/run/workingdirLegacy -u $auth
